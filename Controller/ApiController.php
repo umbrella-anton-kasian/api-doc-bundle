267da6f52b4f3a6fe42e61976687f04e46a02e31
Anton Kasyan <anton.kasian@umbrella-web.com>
2018-05-31 16:09:35 +0300

    add support for nelmio v3 api doc title

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ApiController extends Controller
 {
     /**
      * @param \Symfony\Component\HttpFoundation\Request $Request
-     * @return \App\Jelly\System\JellyResponseItem|\Symfony\Component\HttpFoundation\JsonResponse
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function getJsonAction(Request $Request) {
         if($this->has('nelmio_api_doc.extractor.api_doc_extractor') and class_exists('Nelmio\ApiDocBundle\Annotation\ApiDoc')) {
@@ -26,9 +27,22 @@ class ApiController extends Controller
     }
 
     /**
+     * @param \Symfony\Component\HttpFoundation\Request $Request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getViewAction() {
-        return $this->render('UmbrellaApiDocBundle::index.html.twig');
+    public function getViewAction(Request $Request) {
+
+        if($this->has('nelmio_api_doc.controller.swagger')) {
+            /** @var \Symfony\Component\HttpFoundation\JsonResponse $Response */
+            $Response = $this->get('nelmio_api_doc.controller.swagger')($Request);
+            $extractedDoc = json_decode($Response->getContent(), true);
+
+            $title = isset($extractedDoc['info']['title']) ? $extractedDoc['info']['title'] : 'Api Documentation';
+        } else {
+            $title = 'Api Documentation';
+        }
+
+        $path = $this->generateUrl('UmbrellaApiDocBundle_api_adm_api_json');
+        return $this->render('UmbrellaApiDocBundle::index.html.twig', ['title' => $title, 'path' => $path]);
     }
 }
